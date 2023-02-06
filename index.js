eee2480371fd2adfb71a80af2764c4c1d8c77b3d
Hilel Kinane <hilel.kinane@gmail.com>
2023-02-06 17:18:17 +0100

    updated index.js to import the CompanyRouter

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ import express from "express";
 const app = express();
 import cors from "cors";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
 dotenv.config();
 import { UserRoute } from "./routes/user.js";
+import { CompanyRoute } from "./routes/company.js";
+
+
 
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(cookieParser());
+
 
 
 app.use('/user', UserRoute)
+app.use('/company', CompanyRoute)
+
 
 
 app.listen(process.env.PORT, () => {
