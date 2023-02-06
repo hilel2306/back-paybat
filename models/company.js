72e0f17c15c827b2a22643cd30485a31a9d7042b
Hilel Kinane <hilel.kinane@gmail.com>
2023-02-06 17:16:59 +0100

    create company.js model

---
+++ models/company.js
@@ -0,0 +1,19 @@
+import { pool } from "../util/database.js";
+
+
+
+export const getCompany = async (id) => {
+    const [rows] = await pool.query(`
+    SELECT * FROM company WHERE id = ?`, [id])
+    return rows[0]
+}
+export const createCompany = async (userEmail, legalstatus, name, adress, phone) => {
+    const [result] = await pool.query(`
+    INSERT INTO company (legalstatus, name, adress, phone)
+    VALUES (?, ?, ?, ?)
+    `, [legalstatus, name, adress, phone]
+    )
+    const id = result.insertId
+    await pool.query(`UPDATE user SET company_id = ? WHERE email = ?`, [id, userEmail])
+    return getCompany(id)
+}
