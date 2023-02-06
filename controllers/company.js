877e13659e99414561b63cb220ae4761a6f28cb7
Hilel Kinane <hilel.kinane@gmail.com>
2023-02-06 21:33:53 +0100

    update to add an email to company

--- controllers/company.js
+++ controllers/company.js
@@ -1,10 +1,11 @@
 import { createCompany } from "../models/company.js";
 
 
+
 export const companyRegistration = async (req, res, next) => {
-    const { userEmail, legalstatus, name, adress, phone } = req.body
+    const { userEmail, legalstatus, name, email, adress, phone } = req.body
     try {
-        const company = await createCompany(userEmail, legalstatus, name, adress, phone);
+        const company = await createCompany(userEmail, legalstatus, name, email, adress, phone);
         return res.send(company)
     } catch (error) {
         console.error(error.message);
--- models/company.js
+++ models/company.js
@@ -7,13 +7,14 @@ export const getCompany = async (id) => {
     SELECT * FROM company WHERE id = ?`, [id])
     return rows[0]
 }
-export const createCompany = async (userEmail, legalstatus, name, adress, phone) => {
+export const createCompany = async (userEmail, legalstatus, name, email, adress, phone) => {
     const [result] = await pool.query(`
-    INSERT INTO company (legalstatus, name, adress, phone)
-    VALUES (?, ?, ?, ?)
-    `, [legalstatus, name, adress, phone]
+    INSERT INTO company (legalstatus, name, email, adress, phone)
+    VALUES (?, ?, ?, ?, ?)
+    `, [legalstatus, name, email, adress, phone]
     )
     const id = result.insertId
     await pool.query(`UPDATE user SET company_id = ? WHERE email = ?`, [id, userEmail])
     return getCompany(id)
 }
+
