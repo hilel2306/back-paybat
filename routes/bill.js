ee5817e68df9dc9069be470b15f9048e396babc8
Hilel Kinane <hilel.kinane@gmail.com>
2023-02-09 12:33:08 +0100

    <feat> add the route to get juste one bill

--- routes/bill.js
+++ routes/bill.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isLoggedIn } from "../controllers/user.js";
-import { billRegistration, getAllbills, saveBill } from "../controllers/bill.js";
+import { billRegistration, getAllbills, getBill, saveBill } from "../controllers/bill.js";
 
 
 
@@ -8,6 +8,7 @@ import { billRegistration, getAllbills, saveBill } from "../controllers/bill.js"
 const router = express.Router();
 
 router.get('/get', isLoggedIn, getAllbills);
+router.get('/get', isLoggedIn, getBill);
 router.post('/registration', isLoggedIn, billRegistration);
 router.put('/save', isLoggedIn, saveBill);
 
