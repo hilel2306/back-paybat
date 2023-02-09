15dafd3104d79d3c6a254162f80588c7c45c8179
Hilel Kinane <hilel.kinane@gmail.com>
2023-02-09 11:21:53 +0100

    added a function to get all bills and an another to save the modifications

--- controllers/bill.js
+++ controllers/bill.js
@@ -1,11 +1,31 @@
-import { createBill } from "../models/bill.js";
+import { createBill, getBills, updateBill } from "../models/bill.js";
 
 export const billRegistration = async (req, res, next) => {
     const { status, title, companyId } = req.body
     try {
         const bill = await createBill(status, title, companyId);
-        return res.send(bill)
+        return res.status(201).json(bill)
     } catch (error) {
         console.error(error.message);
+    }
+}
+
+export const getAllbills = async (req, res) => {
+    const companyId = req.query.companyId
+    try {
+        const bills = await getBills(companyId)
+        return res.status(201).json(bills)
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+export const saveBill = async (req, res) => {
+    try {
+        const bill = await updateBill(req.body)
+        return res.status(201).json(bill)
+
+    } catch (error) {
+        console.error(error.message)
     }
 }
