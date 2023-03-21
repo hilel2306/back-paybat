6c8d68767a9948cd52ab53bc79d925457790a7fa
Hilel Kinane <hilel.kinane@gmail.com>
2023-03-21 11:06:01 +0100

    <refactor> we don't use this controller for the moment

--- controllers/company.js
+++ controllers/company.js
@@ -1,13 +1,17 @@
-import { createCompany } from "../models/company.js";
+import { createCompany, findCompanyByEmail } from "../models/company.js";
 
 
 
-export const companyRegistration = async (req, res, next) => {
-    const { userEmail, legalstatus, name, email, adress, phone } = req.body
-    try {
-        const company = await createCompany(userEmail, legalstatus, name, email, adress, phone);
-        return res.send(company)
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+// export const companyRegistration = async (req, res, next) => {
+//     const { userEmail, legalstatus, name, email, adress, phone } = req.body
+//     try {
+//         const companyExists = await findCompanyByEmail(email);
+//         if (!companyExists) {
+//             const company = await createCompany(userEmail, legalstatus, name, email, adress, phone);
+//             return res.send(company)
+//         }
+//         return res.send("This company already exists")
+//     } catch (error) {
+//         console.error(error.message);
+//     }
+// }
